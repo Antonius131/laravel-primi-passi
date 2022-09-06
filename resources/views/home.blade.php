d07aefad1e0dcf961c4833806246834d63201c55
Antonius131 <antomendetta@gmail.com>
2022-09-06 15:39:08 +0200

    creating new home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>New Laravel Home</title>
+   <!-- Font -->
+   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+   <!-- Style -->
+   <style>
+      html,
+      body {
+         background-color: #fff;
+         color: #636b6f;
+         font-family: 'Nunito', sans-serif;
+         font-weight: 200;
+         height: 100vh;
+         margin: 0;
+      }
+
+      .flex-center {
+         align-items: center;
+         display: flex;
+         justify-content: center;
+      }
+   </style>
+</head>
+<body>
+   <div class="flex-center">
+      <h1>Hello Laravel!</h1>
+   </div>
+</body>
+</html>
